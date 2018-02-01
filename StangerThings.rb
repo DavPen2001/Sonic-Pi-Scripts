12807d74770804029c6015934fdc0c8c91b30f55
DavPen2001 <david_rodriguez@davincischools.org>
2018-02-01 13:38:36 -0800

    Add files via upload

---
+++ StangerThings.rb
@@ -0,0 +1,188 @@
+use_bpm 160
+
+use_synth :saw
+with_fx :band_eq do
+  live_loop :strange do
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :b2
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c4
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    
+    play :b2
+    sleep 0.5
+    play :e3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    
+    play :c3
+    sleep 0.5
+    play :b3
+    sleep 0.5
+    play :g3
+    sleep 0.5
+    play :e3
+    sleep 0.5
+  end
+end
+
+
